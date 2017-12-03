9213268ab274187f618a3f8182de5fcb50f9b2d4
Oolga <olgamikhalenka97@gmail.com>
2017-12-03 22:49:33 +0300

    webdriver

---
+++ xUnit_WebDriver/xUnit_WebDriver/GithubTest.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace xUnit_WebDriver
+{
+    public class GithubTest
+    {
+        private string USERNAME = "testautomationuser";
+        private string PASSWORD = "Time4Death!";
+        private string URL = "https://github.com/";
+
+        [Fact]
+        public void LogIn()
+        {
+            using (var driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl(URL);
+
+                var loginButton = driver.FindElementByXPath("//a[text() = 'Sign in']");
+                loginButton.Click();
+
+                var userNameField = driver.FindElementById("login_field");
+                var userPasswordField = driver.FindElementById("password");
+
+                userNameField.SendKeys(USERNAME);
+                userPasswordField.SendKeys(PASSWORD);
+            }
+        }
+    }
+}
---
+++ xUnit_WebDriver/xUnit_WebDriver/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("xUnit_WebDriver")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("xUnit_WebDriver")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("5d11901c-61dc-4b27-bf9e-184d11779dd8")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
